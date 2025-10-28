5295f4558ca9119964297c9f2ee07ebf4cbcb8a2
Robert Melton <rsmelton@crimson.ua.edu>
2025-10-28 15:30:14 -0500

    The game is now significantly cleaner and handles data much more efficiently. We also have some accessibility improvements.

--- src/components/Dice/DiceContainer.tsx
+++ src/components/Dice/DiceContainer.tsx
@@ -1,62 +1,136 @@
 import { useState, useRef, useEffect } from "react";
+import { nanoid } from "nanoid";
 import Die from "./Die";
 
 const DiceContainer = () => {
-  const [diceNumbers, setDiceNumbers] = useState<number[]>([]);
-  const [amountDiceClicked, setAmountDiceClicked] = useState<number>(0);
-  const [hasResetGame, setHasResetGame] = useState<boolean>(false);
+  // This is called Lazy state initialization and only calls 
+  // generateAllNewDice once for the initial state
+  const [dice, setDice] = useState(() => generateAllNewDice());
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
-  const chosenNumber = useRef<number>(0);
+  let gameWon =
+    dice.every((die) => die.isHeld) &&
+    dice.every((die) => die.value === dice[0].value);
 
-  const rollDice = () => {
-    const arr: number[] = Array.from({ length: 10 }, () => {
-      return Math.floor(Math.random() * 6) + 1;
-    });
-    setDiceNumbers(arr);
-  };
+    useEffect(() => {
+        if (gameWon) {
+            buttonRef.current?.focus();
+        }
+    }, [gameWon]);
 
-  const resetGame = () => {
-    // When this method runs, we want to do a few things:
-    // 1. Unlock all the die (The die themselves take care of this once hasResetGame is true)
-    setHasResetGame(true);
-    // 2. Call method to get ten random numbers for the new die
-    rollDice();
-    // 3. Set amountDiceClicked back to 0
-    setAmountDiceClicked(0);
-    // 4. Reset the chosenNumber
-    chosenNumber.current = 0;
-  };
+  function generateAllNewDice() {
+    return new Array(10).fill(0).map(() => ({
+      id: nanoid(),
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+    }));
+  }
 
-  // re-render so we can set the new rolled numbers
-  useEffect(() => {
-    rollDice();
-  }, []);
+  function resetGame() {
+    setDice(generateAllNewDice);
+  }
+
+  function rollDice() {
+    setDice((prevDice) =>
+      prevDice.map((die) =>
+        die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }
+      )
+    );
+  }
+
+  function hold(id: string) {
+    setDice((prevDice) =>
+      prevDice.map((die) =>
+        die.id === id ? { ...die, isHeld: !die.isHeld } : die
+      )
+    );
+  }
+
+  const diceElements = dice.map((dieObj) => (
+    <Die
+      key={dieObj.id}
+      id={dieObj.id}
+      value={dieObj.value}
+      isHeld={dieObj.isHeld}
+      hold={hold}
+    />
+  ));
 
   return (
     <div className="flex flex-col items-center gap-8">
-      <div className="grid grid-cols-5 gap-x-4 gap-y-4">
-        {diceNumbers &&
-          diceNumbers.map((number, index) => (
-            <Die
-              key={index}
-              number={number}
-              chosenNumber={chosenNumber}
-              amountDiceClicked={amountDiceClicked}
-              setAmountDiceClicked={setAmountDiceClicked}
-              hasResetGame={hasResetGame}
-              setHasResetGame={setHasResetGame}
-            />
-          ))}
-      </div>
-
+      <div className="grid grid-cols-5 gap-x-4 gap-y-4">{diceElements}</div>
       <button
-        onClick={amountDiceClicked !== 10 ? rollDice : resetGame}
+        ref={buttonRef}
         className="text-white bg-[#5035FF] px-8 py-2 rounded cursor-pointer hover:bg-[#5035DD]"
+        onClick={gameWon ? resetGame : rollDice}
       >
-        {amountDiceClicked !== 10 ? "Roll" : "Play again"}
+        {gameWon ? "Play again" : "Roll"}
       </button>
     </div>
   );
 };
 
 export default DiceContainer;
+
+// import { useState, useRef, useEffect } from "react";
+// import Die from "./Die";
+
+// const DiceContainer = () => {
+//   const [diceNumbers, setDiceNumbers] = useState<number[]>([]);
+//   const [amountDiceClicked, setAmountDiceClicked] = useState<number>(0);
+//   const [hasResetGame, setHasResetGame] = useState<boolean>(false);
+
+//   const chosenNumber = useRef<number>(0);
+
+//   const rollDice = () => {
+//     const arr: number[] = Array.from({ length: 10 }, () => {
+//       return Math.floor(Math.random() * 6) + 1;
+//     });
+//     setDiceNumbers(arr);
+//   };
+
+//   const resetGame = () => {
+//     // When this method runs, we want to do a few things:
+//     // 1. Unlock all the die (The die themselves take care of this once hasResetGame is true)
+//     setHasResetGame(true);
+//     // 2. Call method to get ten random numbers for the new die
+//     rollDice();
+//     // 3. Set amountDiceClicked back to 0
+//     setAmountDiceClicked(0);
+//     // 4. Reset the chosenNumber
+//     chosenNumber.current = 0;
+//   };
+
+//   // re-render so we can set the new rolled numbers
+//   useEffect(() => {
+//     rollDice();
+//   }, []);
+
+//   return (
+//     <div className="flex flex-col items-center gap-8">
+//       <div className="grid grid-cols-5 gap-x-4 gap-y-4">
+//         {diceNumbers &&
+//           diceNumbers.map((number, index) => (
+//             <Die
+//               key={index}
+//               number={number}
+//               chosenNumber={chosenNumber}
+//               amountDiceClicked={amountDiceClicked}
+//               setAmountDiceClicked={setAmountDiceClicked}
+//               hasResetGame={hasResetGame}
+//               setHasResetGame={setHasResetGame}
+//             />
+//           ))}
+//       </div>
+
+//       <button
+//         onClick={amountDiceClicked !== 10 ? rollDice : resetGame}
+//         className="text-white bg-[#5035FF] px-8 py-2 rounded cursor-pointer hover:bg-[#5035DD]"
+//       >
+//         {amountDiceClicked !== 10 ? "Roll" : "Play again"}
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default DiceContainer;
--- src/components/Dice/Die.tsx
+++ src/components/Dice/Die.tsx
@@ -1,60 +1,87 @@
-import { useState, useEffect } from "react";
-import type { FC, RefObject, Dispatch } from "react";
+import type { FC } from "react";
 
 type DieProps = {
-  number: number;
-  chosenNumber: RefObject<number>;
-  amountDiceClicked: number;
-  setAmountDiceClicked: Dispatch<React.SetStateAction<number>>;
-  hasResetGame: boolean;
-  setHasResetGame: Dispatch<React.SetStateAction<boolean>>;
-};
+    id: string;
+    value: number;
+    isHeld: boolean;
+    hold: (id: string) => void;
+}
 
-const Die: FC<DieProps> = ({
-  number,
-  chosenNumber,
-  amountDiceClicked,
-  setAmountDiceClicked,
-  hasResetGame,
-  setHasResetGame,
-}) => {
-  const [hasBeenClicked, setHasBeenClicked] = useState(false);
-
-  useEffect(() => {
-    console.log("Do we enter has reset game useEffect hook");
-    if (hasResetGame) {
-        setHasBeenClicked(false);
-        setHasResetGame(false);
-    }
-  }, [hasResetGame]);
-
-  useEffect(() => {
-    // If the amount of die clicked = 10 then we should play celebration animation
-    console.log("Amount of Die Clicked: ", amountDiceClicked);
-  }, [amountDiceClicked]);
-
-  const handleClick = () => {
-    if (chosenNumber.current === 0) { // user has never clicked a die so set the chosenNumber
-      setHasBeenClicked(true);
-      chosenNumber.current = number;
-      setAmountDiceClicked((prevAmountDiceClicked) => prevAmountDiceClicked + 1);
-    } else if (chosenNumber.current === number) { // able to be clicked
-      setHasBeenClicked(true);
-      setAmountDiceClicked((prevAmountDiceClicked) => prevAmountDiceClicked + 1);
-    }
-  };
-
+const Die: FC<DieProps> = ({ id, value, isHeld, hold }) => {
   return (
-    // Only allow click if chosenNumber has been set
-    <div
-      onClick={hasBeenClicked ? () => {} : handleClick}
+    <button
       className={`${
-        hasBeenClicked ? "bg-[#59E391]" : "bg-[#FFFFFF] cursor-pointer"
-      } px-4 py-2 rounded shadow-[0px_2px_2px_0px_rgba(0,0,0,0.15)]`}
+        isHeld ? "bg-[#59E391]" : "bg-white"
+      } px-4 py-2 rounded cursor-pointer shadow-[0px_2px_2px_0px_rgba(0,0,0,0.15)]`}
+      onClick={() => hold(id)}
+      aria-pressed={isHeld}
+      aria-label={`Die with value ${value}, ${isHeld ? "held" : "not held"}`}
     >
-      {hasBeenClicked ? chosenNumber.current : number}
-    </div>
+      {value}
+    </button>
   );
 };
 
 export default Die;
+
+
+// import { useState, useEffect } from "react";
+// import type { FC, RefObject, Dispatch } from "react";
+
+// type DieProps = {
+//   number: number;
+//   chosenNumber: RefObject<number>;
+//   amountDiceClicked: number;
+//   setAmountDiceClicked: Dispatch<React.SetStateAction<number>>;
+//   hasResetGame: boolean;
+//   setHasResetGame: Dispatch<React.SetStateAction<boolean>>;
+// };
+
+// const Die: FC<DieProps> = ({
+//   number,
+//   chosenNumber,
+//   amountDiceClicked,
+//   setAmountDiceClicked,
+//   hasResetGame,
+//   setHasResetGame,
+// }) => {
+//   const [hasBeenClicked, setHasBeenClicked] = useState(false);
+
+//   useEffect(() => {
+//     console.log("Do we enter has reset game useEffect hook");
+//     if (hasResetGame) {
+//         setHasBeenClicked(false);
+//         setHasResetGame(false);
+//     }
+//   }, [hasResetGame]);
+
+//   useEffect(() => {
+//     // If the amount of die clicked = 10 then we should play celebration animation
+//     console.log("Amount of Die Clicked: ", amountDiceClicked);
+//   }, [amountDiceClicked]);
+
+//   const handleClick = () => {
+//     if (chosenNumber.current === 0) { // user has never clicked a die so set the chosenNumber
+//       setHasBeenClicked(true);
+//       chosenNumber.current = number;
+//       setAmountDiceClicked((prevAmountDiceClicked) => prevAmountDiceClicked + 1);
+//     } else if (chosenNumber.current === number) { // able to be clicked
+//       setHasBeenClicked(true);
+//       setAmountDiceClicked((prevAmountDiceClicked) => prevAmountDiceClicked + 1);
+//     }
+//   };
+
+//   return (
+//     // Only allow click if chosenNumber has been set
+//     <div
+//       onClick={hasBeenClicked ? () => {} : handleClick}
+//       className={`${
+//         hasBeenClicked ? "bg-[#59E391]" : "bg-[#FFFFFF] cursor-pointer"
+//       } px-4 py-2 rounded shadow-[0px_2px_2px_0px_rgba(0,0,0,0.15)]`}
+//     >
+//       {hasBeenClicked ? chosenNumber.current : number}
+//     </div>
+//   );
+// };
+
+// export default Die;
